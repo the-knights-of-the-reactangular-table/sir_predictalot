e36e7888fc5fa3f2d7a9b31770e47e3e13009806
Rory Sedgwick <rory.sedgwick@gmail.com>
2015-03-31 15:19:38 +0100

    tinder app basic layout

--- src/main.js
+++ src/main.js
@@ -1,5 +1,67 @@
 var React = require("react");
 
+var MainBox = React.createClass({
+    render: function() {
+        return(
+        <div className="mainBox">
+            Mainbox
+            <PredictionBox />
+            <MenuBox />
+        </div>
+        );
+    }
+});
+
+
+var PredictionBox = React.createClass({
+    render: function() {
+        return(
+            <div className="predictionBox">
+                PredictionBox
+                <ImageBox />
+                <TextBox />
+            </div>
+        );
+    }
+});
+
+
+var MenuBox = React.createClass({
+    render: function() {
+        return(
+            <div className="menuBox">
+                MenuBox
+            </div>
+            );
+    }
+});
+
+
+var TextBox = React.createClass({
+    render: function() {
+        return(
+            <div className="textBox">
+                TextBox
+            </div>
+            );
+    }
+});
+
+
+var ImageBox= React.createClass({
+    render: function() {
+        return(
+            <div className="imageBox">
+                ImageBox
+            </div>
+            );
+    }
+});
+
+
+
+
 React.render(
-	<div>hi m8</div>, document.getElementById("content")
-	);
+    <MainBox />,
+    document.getElementById("content")
+    );

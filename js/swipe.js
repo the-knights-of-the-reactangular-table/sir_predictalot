d39df68b71b4a1407ddd64a50df630f551c9aedc
perborgen <perhb@hotmail.com>
2015-04-01 13:27:54 +0100

    touch changes

---
+++ js/swipe.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+
+    $(".yes").on("click",function(){
+      console.log('clicked');
+      
+      $(this).addClass('rotate-left').delay(700).fadeOut(1);
+      $('.buddy').find('.status').remove();
+
+      $(this).append('<div class="status like">Like!</div>');      
+      if ( $(this).is(':last-child') ) {
+        $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
+       } else {
+          $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
+       }
+    });  
+
+   $(".no").on("swipeleft",function(){
+    $(this).addClass('rotate-right').delay(700).fadeOut(1);
+    $('.buddy').find('.status').remove();
+    $(this).append('<div class="status dislike">Dislike!</div>');
+
+    if ( $(this).is(':last-child') ) {
+     $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
+      alert('Na-na!');
+     } else {
+        $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
+    } 
+  });
+
+});
--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
 var React = require("react");
 
-var MainBox = React.createClass({
+React.initializeTouchEvents(true);
+
+var DATA = [{url: 'http://dummyimage.com/300', text: 'Will this couple break up this week 1?'}];
+
+var MainBox = React.createClass({ 
+
+    componentDidMount: function(){
+        this.setState({
+            data: DATA[0]
+        });
+    },
     render: function() {
         return(
         <div className="mainBox">
-            Mainbox
-            <PredictionBox />
+            Predict the News
+            <button  />     
+            <PredictionBox data={this.props.data} />
             <MenuBox />
         </div>
         );
@@ -14,12 +25,13 @@ var MainBox = React.createClass({
 
 
 var PredictionBox = React.createClass({
+
     render: function() {
         return(
             <div className="predictionBox">
-                PredictionBox
-                <ImageBox />
-                <TextBox />
+                <TextBox text={this.props.data.text}/>
+                <ImageBox url={this.props.data.url} />
+                
             </div>
         );
     }
@@ -30,7 +42,7 @@ var MenuBox = React.createClass({
     render: function() {
         return(
             <div className="menuBox">
-                MenuBox
+                My Profile - More info - Settings
             </div>
             );
     }
@@ -41,18 +53,40 @@ var TextBox = React.createClass({
     render: function() {
         return(
             <div className="textBox">
-                TextBox
+                {this.props.text}
             </div>
             );
     }
 });
 
 
-var ImageBox= React.createClass({
+var ImageBox = React.createClass({
+
+    handleTouchMove: function(e){
+        lastX = e.touches[0].pageX;
+        this.props.lastX = lastX;
+
+    },
+
+    handleTouchStart: function(e){
+        console.log('touch start');
+        firstX = e.touches[0].pageX;
+        this.props.firstX = firstX;
+    },
+
+    onTouchEnd: function(e){
+        if(this.props.firstX - this.props.lastX > 75) {
+            console.log("swipe left");
+        } else if (this.props.firstX - this.props.lastX < -75) {
+            console.log("swipe right")
+        }
+    },
     render: function() {
         return(
-            <div className="imageBox">
-                ImageBox
+            <div className="imageBox" className="predictionImg" >
+                <img src={this.props.url} onTouchMove={this.handleTouchMove} onTouchEnd={this.onTouchEnd} onTouchStart={this.handleTouchStart} />
+                <span className="yes" >Yes</span>
+                <span className="No" >No</span>
             </div>
             );
     }
@@ -62,6 +96,6 @@ var ImageBox= React.createClass({
 
 
 React.render(
-    <MainBox />,
+    <MainBox data={DATA[0]} />,
     document.getElementById("content")
     );

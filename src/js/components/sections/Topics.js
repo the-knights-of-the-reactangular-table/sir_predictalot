1b3e50755f08972a85e41063a091c5fcb05b41bc
James <james.griffiths.2222@googlemail.com>
2015-04-01 16:32:44 +0100

    minor work

--- src/js/components/sections/Topics.js
+++ src/js/components/sections/Topics.js
@@ -1,19 +1,19 @@
 var React = require("react");
 
 var Topics = React.createClass({
-	render: function() {
-		return (
-			<div className={"sectionHolder " + this.props.topic} id="Topics">
-				<div className="sectionHeader">
-					<h1>Topics</h1>
-				</div>
-				<div className="sectionBody">
-					<div className="section sectionLeft"></div>
-					<div className="section sectionRight"></div>
-				</div>
-			</div>
-		);
-	}
+    render: function() {
+        return (
+            <div className={"sectionHolder " + this.props.topic} id="Topics">
+                <div className="sectionHeader">
+                    <h1>Topics</h1>
+                </div>
+                <div className="sectionBody">
+                    <div className="section sectionSingle">
+                    </div>
+                </div>
+            </div>
+        );
+    }
 });
 
 module.exports = Topics;

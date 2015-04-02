4b81a1da85cd8919873d5f15a49adc4150bbe34f
Rory Sedgwick <rory.sedgwick@gmail.com>
2015-04-02 15:39:15 +0100

    serving files and files restructure

--- server.js
+++ server.js
@@ -1,12 +1,43 @@
-var http = require("http");
-var port = 4000;
-var fs = require('fs');
+var Hapi = require('hapi');
 
-var server = http.createServer(function (req,res){
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end(fs.readFileSync('index.html'));
+var server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.route({
+    method: "GET",
+    path: "/",
+    handler: function(request, reply) {
+        reply.file("/Users/rorysedgwick/Projects/fac4/week10/sir_predictalot/index.html");
+    },
 });
 
-server.listen(port);
+server.route({
+    method: "GET",
+    path: "/build/main.js",
+    handler: function(request, reply) {
+        reply.file("/Users/rorysedgwick/Projects/fac4/week10/sir_predictalot/build/main.js");
+    },
+});
 
-console.log('Server running on port ', port);
+server.route({
+    method: "GET",
+    path: "/build/index.css",
+    handler: function(request, reply) {
+        reply.file("/Users/rorysedgwick/Projects/fac4/week10/sir_predictalot/build/index.css");
+    },
+});
+
+// server.route({
+//     method: "GET",
+//     path: "/{param}",
+//     handler: {
+//         directory: {
+//             path: "/Users/rorysedgwick/Projects/fac4/week10/sir_predictalot/build/"
+//         }
+//     },
+// });
+
+server.start(function() {
+    console.log('Server running at:', server.info.uri);
+});
+
--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 //var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 React.initializeTouchEvents(true);
 
-var DATA = [{url: 'img/zlatan.jpg', text: 'Will PSG win Champions League?', key: 1, animation_class: ""},
+var DATA = [{url: '../build/zlatan.jpg', text: 'Will PSG win the Champions League?', key: 1, animation_class: ""},
             {url: 'http://lorempixel.com/g/400/400/people', text: "Will he win Eurovision 2015?", key: 2, animation_class: ""},
             {url: 'http://lorempixel.com/g/400/400/people', text: 'Will they break up?', key: 3, animation_class: ""}
 ];
 
-var MainBox = React.createClass({ 
+var MainBox = React.createClass({
 
     handleTouchMove: function(e){
         lastX = e.touches[0].pageX;
@@ -45,9 +45,9 @@ var MainBox = React.createClass({
         new_active = image_number - 1;
         console.log('new_active_text: ', new_active);
 
-        this.setState({ 
+        this.setState({
             data : newData,
-            active : new_active 
+            active : new_active
         });
 
 
@@ -65,10 +65,10 @@ var MainBox = React.createClass({
     render: function() {
         return(
         <div className="mainBox">
-            <PredictionBox  active={this.state.active} 
-                            data={this.props.data} 
-                            onTouchEnd={this.onTouchEnd} 
-                            handleTouchStart={this.handleTouchStart} 
+            <PredictionBox  active={this.state.active}
+                            data={this.props.data}
+                            onTouchEnd={this.onTouchEnd}
+                            handleTouchStart={this.handleTouchStart}
                             handleTouchMove={this.handleTouchMove}/>
             <MenuBox />
         </div>
@@ -82,10 +82,10 @@ var PredictionBox = React.createClass({
         return(
             <div className="predictionBox">
                 <TextBox data={this.props.data} active={this.props.active}/>
-                <ImageBox data={this.props.data} 
-                            onTouchEnd={this.props.onTouchEnd} 
-                            handleTouchStart={this.props.handleTouchStart} 
-                            handleTouchMove={this.props.handleTouchMove}/>         
+                <ImageBox data={this.props.data}
+                            onTouchEnd={this.props.onTouchEnd}
+                            handleTouchStart={this.props.handleTouchStart}
+                            handleTouchMove={this.props.handleTouchMove}/>
             </div>
         );
     }
@@ -101,10 +101,10 @@ var MenuBox = React.createClass({
             );
     }
 });
-    
- 
+
+
 var TextBox = React.createClass({
-    render: function() { 
+    render: function() {
         console.log('checkin if else')
         if (this.props.active < 0) {
             return(
@@ -131,22 +131,22 @@ var ImageBox = React.createClass({
         var images = this.props.data.map(function(image, i){
            return (
                 <div>
-                    <div key={that.props.data.key} 
-                        className={image.animation_class} 
-                        onTouchMove={that.props.handleTouchMove} 
-                        onTouchEnd={that.props.onTouchEnd.bind(null,image)} 
-                        onTouchStart={that.props.handleTouchStart}> 
+                    <div key={that.props.data.key}
+                        className={image.animation_class}
+                        onTouchMove={that.props.handleTouchMove}
+                        onTouchEnd={that.props.onTouchEnd.bind(null,image)}
+                        onTouchStart={that.props.handleTouchStart}>
                     <img className="predictionImg" src={image.url} /></div>
                 </div>
             );
         });
 
-       
+
         return(
             <div className="imageBox" >
             <ReactCSSTransitionGroup transitionName="swipe">
 
-                {images}  
+                {images}
             </ReactCSSTransitionGroup>
 
             </div>

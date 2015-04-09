1d700e36fe80ea5da7efb3129db7a435808537ca
Rory Sedgwick <rory.sedgwick@gmail.com>
2015-04-09 19:55:24 +0100

    change port for heroku

--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ var Hapi 	= require('hapi');
 var server 	= new Hapi.Server();
 
 server.connection({
-	host: 'localhost',
+	host:  process.env.HOST	|| 'localhost',
 	port: process.env.PORT || 8000
 });
 

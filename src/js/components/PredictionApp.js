af86d9e4844604af0eac828b980daddd74037cfb
perborgen <perhb@hotmail.com>
2015-04-09 11:00:23 +0100

    fixes

--- src/js/components/PredictionApp.js
+++ src/js/components/PredictionApp.js
@@ -3,7 +3,6 @@ var PredictionSection 	= require("./sections/Prediction");
 var ChallengeSection 	= require("./sections/Challenge");
 var PredictionSpinner 	= require("./sections/PredictionSpinner");
 var AppStore 			= require("../stores/AppStore");
-var SubmissionSection 	= require("../sections/Submission");
 
 function getStateFromStores() {
 	var currentUser  	 = AppStore.getUser();
@@ -47,38 +46,16 @@ var PredictionApp = React.createClass({
 	render: function() {
 		console.log('user in PredictionApp: ', this.state.user);
 		console.log('data in PredictionApp: ', this.state.data);
-		var preferences = this.state.user.preferences;
-		var sections = preferences.sections.map(function(ele, ind) {
-				switch(ele) {
-
-					case "challenges":
-						return <ChallengeSection key={ele}/>;
-
-					default:
-						return;
-				}
-		}, this);
 		
-		if (isOnPredictionPage){
+
+
 			return (
 					<div>
 						<PredictionSection currentEventName={this.state.currentEventName} predictionId={this.state.predictionId} prediction={this.state.current_pred} data={this.state.data} active={this.state.active}/>;
-						{sections}
 					</div>
 				);
-		}
-		else if (isOnSubmissionPage){
-			return (
-					<div>
-						<SubmissionSection />
-					</div>
-				);
-		}
-
-	
-
-
 	},
+
 
 	_onChange: function() {
 		this.setState(getStateFromStores());
